16790859293efdf78924ec428d71762b53728013
sanath <ramsaanath@gmail.com>
2019-06-28 09:28:38 +0530

    Changed the package name.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Kamakoti
-The official mobile application for Kanchi Kamakoti Peetam
+# kamakoti
+
+Official mobile application for Sri Kanchi Kamakoti Peetam
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our 
+[online documentation](https://flutter.dev/docs), which offers tutorials, 
+samples, guidance on mobile development, and a full API reference.
--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
 import 'package:redux_logging/redux_logging.dart';
+import 'package:http/http.dart' as http;
 
-import 'package:sri_kamakoti/logic/actions.dart';
 import 'package:sri_kamakoti/logic/reducers.dart';
 import 'package:sri_kamakoti/logic/middlewares.dart';
 import 'package:sri_kamakoti/models/app_state.dart';
 import 'package:sri_kamakoti/screens/home_screen.dart';
+import 'package:background_fetch/background_fetch.dart';
+
+const EVENTS_KEY = "fetch_events";
+
+/// This "Headless Task" is run when app is terminated.
+void backgroundFetchHeadlessTask() async {
+  print('[BackgroundFetch] Headless event received.');
+
+  var apiUrl =
+      "https://us-central1-srikamakoti-822a3.cloudfunctions.net/addMessage?text=" +
+          DateTime.now().toString();
+  await http.get(apiUrl);
+
+  BackgroundFetch.finish();
+}
 
 void main() {
   runApp(App());
+
+  // Register to receive BackgroundFetch events after app is terminated.
+  // Requires {stopOnTerminate: false, enableHeadless: true}
+  BackgroundFetch.registerHeadlessTask(backgroundFetchHeadlessTask);
 }
 
 class App extends StatelessWidget {
@@ -26,6 +45,19 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    BackgroundFetch.configure(
+            BackgroundFetchConfig(
+                minimumFetchInterval: 15,
+                stopOnTerminate: false,
+                enableHeadless: true,
+                forceReload: false),
+            backgroundFetchHeadlessTask)
+        .then((int status) {
+      print('[BackgroundFetch] SUCCESS: $status');
+    }).catchError((e) {
+      print('[BackgroundFetch] ERROR: $e');
+    });
+
     return StoreProvider<AppState>(
       store: store,
       child: MaterialApp(
